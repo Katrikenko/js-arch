4628e616610f5dc5a6363499ec4fad6502626525
Kateryna Lysenko <katrikenko@gmail.com>
2023-08-23 10:56:24 +0200

    js architecture of folders

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,18 @@
-import '../../script/test'
+/*
+// import '../../script/test'
+
+// import MyName, { message3 } from '../../script/test'
+import { Hello } from '../../script/test'
+
+// import {
+//   message3 as firstMessage,
+//   message2,
+// } from '../../script/test'
 
 console.log('container')
+// console.log(MyName, message2, firstMessage)
+
+Hello()
+*/
+
+console.log(window.screenX)
---
+++ src/script/test2.js
@@ -0,0 +1,18 @@
+/*
+console.log('script')
+
+// const message = 'Default text'
+
+// const message2 = () => {}
+
+// function Message3() {}
+
+// const message3 = 'script'
+
+// export { message3, Message3, message2 }
+
+// export default message
+
+export default () => console.log('Hello, World!')
+
+*/
